aaec79f4a0d5e69aca077cfa605e612a85769da7
AndreeaTM <andreeateodora.marinica@yahoo.com>
2019-05-18 22:36:08 +0300

    initial commit

---
+++ STRINGS/STRINGS/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace STRINGS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 Write a method to remove the n-th index from a nonempty string
+            string myString = "Some text to get used to strings!";
+            Console.WriteLine(myString.Remove(5, 1));
+
+            // 2 Write a method to remove the characters which have odd index values of a given string.
+           
+
+        }
+    }
+}
