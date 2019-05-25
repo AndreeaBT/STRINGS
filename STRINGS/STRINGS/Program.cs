455035f821a54010b9c27870683581ed7cfc313c
AndreeaTM <andreeateodora.marinica@yahoo.com>
2019-05-25 21:01:45 +0300

    added modifiers

--- STRINGS/STRINGS/Program.cs
+++ STRINGS/STRINGS/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Linq;
+using static System.Net.WebRequestMethods;
 
 namespace STRINGS
 {
@@ -7,22 +8,50 @@ namespace STRINGS
     {
         static void Main(string[] args)
         {
-            // 1 Write a method to remove the n-th index from a nonempty string
-            string myString = "Some text to get used to strings!";
-            Console.WriteLine(myString.Remove(5, 1));
 
-            //Write a method to remove the characters which have odd index values of a given string.
+            RemoveAnIndex();
+            RemoveOddIndexValue();
+            UpperAndLowerInput();
+            ReverseMultipleOfFourString();
+            ReverseString();
+            GetStringFromUser();
+        }
+        public static void GetStringFromUser()
+        {
            
-            //Write a method that takes input from the user and displays that input back in upper and lower cases.
+        }
+        
+        static void RemoveAnIndex()
+        {
+           
+            var myString = "This is a phrase to get used to string manipulation!";
+
+            Console.WriteLine(myString.Remove(5, 12));
+
+        }
+        public static void RemoveOddIndexValue()
+        {
+           
+        }
+        public static void UpperAndLowerInput()
+        {
+             var myString = "This is a phrase to get used to string manipulation!";
             Console.WriteLine(myString.ToLower());
 
             Console.WriteLine(myString.ToUpper());
 
-
-            //Write method to convert a given string to all uppercase if it contains at least 2 uppercase characters in the first 4 characters.
-
-
+        }
+        public static void ReverseMultipleOfFourString()
+        {
+            //var myString = "This is a phrase to get used to string manipulation!";
+            
+        }
+        public static void ReverseString()
+        {
+            var myString = "This is a phrase to get used to string manipulation!";
+            Console.WriteLine(new string(myString.Reverse().ToArray()));
 
         }
-    }
+
+   }
 }
