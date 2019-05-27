bc0cc965c4ce04f76b5b0c28bea5cb3b40b8208d
AndreeaTM <andreeateodora.marinica@yahoo.com>
2019-05-27 15:08:16 +0300

    changes added

--- STRINGS/STRINGS/Program.cs
+++ STRINGS/STRINGS/Program.cs
@@ -16,20 +16,22 @@ namespace STRINGS
             ReverseString();
             
         }
-      
-        
+
+
         static void RemoveAnIndex()
         {
-           
+
             var myString = "This is a phrase to get used to string manipulation!";
 
             Console.WriteLine(myString.Remove(5, 12));
-
         }
+
+
+
         public static void RemoveOddIndexValue()
         {
-            
-            string  myFirst ="This is a string to get used to string manipulation!";
+
+            string myFirst = "This is a string to get used to string manipulation!";
             string mySecond = "";
             char[] Third = myFirst.ToArray();
             for (int i = 0; i < Third.Length; i++)
@@ -38,26 +40,45 @@ namespace STRINGS
                 {
                     mySecond += Third[i].ToString();
                 }
-                
+
             }
             Console.WriteLine(mySecond);
-
-
-
         }
+
+
+
         public static void UpperAndLowerInput()
         {
-             var myString = "This is a phrase to get used to string manipulation!";
+            var myString = "This is a phrase to get used to string manipulation!";
             Console.WriteLine(myString.ToLower());
 
             Console.WriteLine(myString.ToUpper());
-
         }
+
+
+
         public static void ReverseMultipleOfFourString()
         {
-            //var myString = "This is a phrase to get used to string manipulation!";
+            string myFirst = "This ";
+            string mySecond = "";
+            char[] Third = myFirst.ToArray();
             
+            for (int i = 0; i < Third.Length; i++)
+            {
+                if (Third.Length % 4 == 0)
+                {
+                    mySecond += Third[i].ToString();
+                }
+                else 
+                {
+                    Console.WriteLine($"The length of this string is not a mutiple of 4!");
+                }
+            }
+            Console.WriteLine(new string(mySecond.Reverse().ToArray()));
         }
+
+
+
         public static void ReverseString()
         {
             var myString = "This is a phrase to get used to string manipulation!";
